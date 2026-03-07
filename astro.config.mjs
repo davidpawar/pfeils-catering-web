cba7deb1dadba41f3ea8f51ef16c13275c8ae47a
David Horn-Pawar <dpawar@skeltech.de>
2026-03-07 22:29:29 +0100

    Update site URL in configuration and enhance BaseHead component with sitemap link and improved code formatting for better readability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.pfeils-catering.de",
   integrations: [mdx(), sitemap()],
 
   adapter: cloudflare({
